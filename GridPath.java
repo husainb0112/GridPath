33ed4e449b8a784ef936fb9adda417404958bfa7
husainb0112 <husainb@nycstudents.net>
2024-11-13 14:17:32 +0000

    v7

--- GridPath.java
+++ GridPath.java
@@ -22,19 +22,20 @@ public class GridPath {
         int below = Integer.MAX_VALUE;        
         if (row < grid.length - 1) {
             below = grid[row + 1][col];
-            return new Location(row + 1, col);
         }
         int right = Integer.MAX_VALUE;
         if (col < grid[0].length - 1) {
             right = grid[row][col + 1];
-            return new Location(row, col + 1);
-        } else return new Location(row, col + 1);
+        } 
+        if (below < right) return new Location(row + 1, col);
+        return new Location(row, col + 1);
     }
 
     public int sumPath(int row, int col) {
         int sum = grid[row][col];
         while(row < grid.length - 1 || col < grid[0].length - 1) {
             Location loc = getNextLoc(row, col);
+            System.out.println(loc);
             row = loc.getRow();
             col = loc.getCol();
             sum += grid[row][col];
